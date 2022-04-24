1a7da7c6d05ea83a0c7d7ec068963de79a48db9f
Chris Kwakman <c.kwakman@digipen.edu>
2022-04-24 13:21:04 +0200

    Fix AABB vs AABB intersection not working

--- engine/src/Engine/Math/geometry_intersection.hpp
+++ engine/src/Engine/Math/geometry_intersection.hpp
@@ -9,10 +9,14 @@ namespace Math {
 
 	inline bool intersect_aabb_aabb(aabb const& _a1, aabb const& _a2)
 	{
-		return glm::all(
-			glm::greaterThanEqual(_a1.center + _a1.extent, _a2.center - _a2.extent) ||
-			glm::lessThanEqual(_a1.center - _a1.extent, _a2.center + _a2.extent)
-		);
+		glm::vec3 const a1_min = _a1.center - _a1.extent;
+		glm::vec3 const a1_max = _a1.center + _a1.extent;
+		glm::vec3 const a2_min = _a2.center - _a2.extent;
+		glm::vec3 const a2_max = _a2.center + _a2.extent;
+		return
+			(a1_min.x <= a2_max.x && a1_max.x >= a2_min.x) &&
+			(a1_min.y <= a2_max.y && a1_max.y >= a2_min.y) &&
+			(a1_min.z <= a2_max.z && a1_max.z >= a2_min.z);
 	}
 
 }
